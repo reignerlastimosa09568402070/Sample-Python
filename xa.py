9149b0c25b94b873b6b3f5ca883494a33644091c
unknown <reigner.lastimosa.iics@ust.edu.ph>
2022-03-15 20:31:46 -0700

    Sample Python for Quiz

---
+++ xa.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 20:26:48 2022
+
+@author: Reigner
+"""
+
+class Person:
+    
+    def eat():
+        print('I am eating')
+        
+
+p1 = Person.eat()
---
+++ xa2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 20:28:53 2022
+
+@author: Reigner
+"""
+
+class Boy:
+    
+    def Punch():
+        print('I will punch u')
+        
+boy1 = Boy.Punch()
+    
